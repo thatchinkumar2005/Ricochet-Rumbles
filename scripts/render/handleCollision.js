0d7e43905ad14ba2cffe9b6687ea0325572f212f
thatchinkumar2005 <thatchinkumar2005@icloud.com>
2024-05-07 00:43:34 +0530

    SemiRicochet mechanics done

--- scripts/render/handleCollision.js
+++ scripts/render/handleCollision.js
@@ -1,7 +1,9 @@
 import moveBullet from "./Bullet.js";
 
 export default function handleCollision(piece) {
-  let gameOver, absorbed, ricochet;
+  let gameOver = false,
+    absorbed = false,
+    ricochet = false;
   let prevDir;
   console.log(piece);
   const Absorb = ["Tank", "Cannon"];
@@ -18,39 +20,70 @@ export default function handleCollision(piece) {
     const cell = piece.parentElement;
     const bullet = document.querySelector(".bullet");
     cell.removeChild(bullet);
-  }
+  } else {
+    if (type === "Ricochet") {
+      ricochet = true;
+      const bullet = document.querySelector(".bullet");
+      prevDir = bullet.dir_;
+      bullet.remove();
+      const cell = piece.parentElement;
+      const srcLocation = [
+        Number(cell.getAttribute("data-row")),
+        Number(cell.getAttribute("data-col")),
+      ];
+      let orientation = piece.orientation;
+      orientation = Array.from(orientation);
+      orientation = orientation.map((i) => Number(i));
+      const index = orientation.indexOf(prevDir);
+      console.log(index);
+      let dir;
+      switch (index) {
+        case 0:
+          dir = orientation[2];
+          break;
+        case 1:
+          dir = orientation[3];
+          break;
+        case 2:
+          dir = orientation[0];
+          break;
+        case 3:
+          dir = orientation[1];
+      }
+      moveBullet(gameOver, dir, srcLocation);
+    } else if (type === "SemiRicochet") {
+      ricochet = true;
+      const bullet = document.querySelector(".bullet");
+      prevDir = bullet.dir_;
+      bullet.remove();
+      const cell = piece.parentElement;
+      const srcLocation = [
+        Number(cell.getAttribute("data-row")),
+        Number(cell.getAttribute("data-col")),
+      ];
+      let orientation = piece.orientation;
+      orientation = Array.from(orientation);
+      orientation = orientation.map((i) => Number(i));
+      console.log(orientation);
+      const index = orientation.indexOf(prevDir);
+      let dir;
+      console.log(index);
+      switch (index) {
+        case 0:
+          dir = orientation[2];
+          break;
+        case 1:
+          dir = orientation[3];
+          break;
+        default:
+          ricochet = false;
+          absorbed = true;
+          console.log("absorbed");
+          return { gameOver, absorbed, ricochet };
+      }
 
-  if (type === "Ricochet") {
-    ricochet = true;
-    const bullet = document.querySelector(".bullet");
-    prevDir = bullet.dir_;
-    bullet.remove();
-    const cell = piece.parentElement;
-    const srcLocation = [
-      Number(cell.getAttribute("data-row")),
-      Number(cell.getAttribute("data-col")),
-    ];
-    let orientation = piece.orientation;
-    orientation = Array.from(orientation);
-    orientation = orientation.map((i) => Number(i));
-    const index = orientation.indexOf(prevDir);
-    console.log(index);
-    let dir;
-    switch (index) {
-      case 0:
-        dir = orientation[2];
-        break;
-      case 1:
-        dir = orientation[3];
-        break;
-      case 2:
-        dir = orientation[0];
-        break;
-      case 3:
-        dir = orientation[1];
+      moveBullet(gameOver, dir, srcLocation);
     }
-    moveBullet(gameOver, dir, srcLocation);
+    return { gameOver, absorbed, ricochet };
   }
-
-  return { gameOver, absorbed, ricochet };
 }
